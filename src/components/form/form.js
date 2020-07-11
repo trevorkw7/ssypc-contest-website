cfd5b86f3de760454c6cd3bc0c6b6f57cbccb64e
trevorkw7 <54345106+trevorkw7@users.noreply.github.com>
2020-07-11 02:27:46 -0700

    Update form.js

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,42 +1,39 @@
 import React from 'react'
-import { UploadButton,Button ,Label, StyledForm, Input, StyledFieldSet, StyledFormWrapper} from './form.css';
-import {Link} from 'gatsby'
+import { UploadButton, Button, Label, StyledForm, Input, StyledFieldSet, StyledFormWrapper } from './form.css';
+import { Link } from 'gatsby'
 import { navigate } from 'gatsby-link'
 
 function encode(data) {
     const formData = new FormData()
-  
+
     for (const key of Object.keys(data)) {
-      formData.append(key, data[key])
+        formData.append(key, data[key])
     }
-  
+
     return formData
-  }
+}
 
 export default function Form() {
-    
+
 
     return (
         <StyledFormWrapper>
-        <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-        <   input type="hidden" name="form-name" value="contact" />
-           
-        <p>
-            <label>Your Name: <input type="text" name="name"/></label>
-          </p>
-          <p>
-            <label>Your Email: <input type="email" name="email"/></label>
-          </p>
-          <p>
-            <label>Message: <textarea name="message"></textarea></label>
-          </p>
-          <p>
-            <label>Upload <input type="file" name="upload"/></label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
+            <form name="Contact Form" method="POST" data-netlify="true">
+                <input type="hidden" name="form-name" value="Contact Form" />
+                <div>
+                    <label><input type="email" name="email" />Your Email:</label>
+                    
+                </div>
+                <div>
+                    <label><textarea name="message" />Message:</label>
+                    
+                </div>
+                <div>
+                    <label> <input type="file" name="upload" />Upload:</label>
+                   
+                </div>
+                <button type="submit">Send</button>
+            </form>
         </StyledFormWrapper>
     )
 }
