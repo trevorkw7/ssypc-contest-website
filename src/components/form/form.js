3a2d6f321dc3feb8dd2813ff633c5f9b75efa0db
trevorkw7 <54345106+trevorkw7@users.noreply.github.com>
2020-07-11 02:04:46 -0700

    Update form.js

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -12,7 +12,7 @@ function encode(data) {
   
     return formData
   }
-  
+
 export default function Form() {
     const [state, setState] = React.useState({})
 
@@ -37,59 +37,22 @@ export default function Form() {
 
     return (
         <StyledFormWrapper>
-        <StyledForm onSubmit={handleSubmit} name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-            <input type="hidden" name="bot-field" />
-            <input type="hidden" name="form-name" value="contact" />
+        <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+        <   input type="hidden" name="form-name" value="contact" />
            
-                <Label htmlFor="name">Name</Label>
-                <Input type="text" name="name" id="name" required/>
-            
-           
-                <Label htmlFor="email">Email</Label>
-                <Input type="text" name="email" id="email" required/>
-          
-           
-                <Label htmlFor="high-school">Current High School</Label>
-                <Input type="text" name="high-school" id="high-school" required/>
-          
-                <StyledFieldSet>
-                    <legend>Photo Category</legend>
-                        <label>
-                        <input type="radio" name="photo-category" value="Documentary" required/>Documentary
-                        </label>
-                        <label>
-                        <input type="radio" name="photo-category" value="Landscape"/>Landscape
-                        </label>
-                        <label>
-                        <input type="radio" name="photo-category" value="Portrait"/>Portrait
-                        </label>
-                        <label>
-                        <input type="radio" name="photo-category" value="Still Life"/>Still Life
-                        </label>
-                        <label>
-                        <input type="radio" name="photo-category" value="Abstract"/>Abstract
-                        </label>  
-                </StyledFieldSet>
-
-                <Label htmlFor="upload">Upload (JPG or PNG, Up to 25MB) </Label>
-                <input onChange={renderPreview} accept=".png, .PNG, .jpg, .JPG, .jpeg, .JPEG" style={{fontSize: '1.5rem',padding: '20px 20px 20px 0px'}} type="file" name="upload" id="upload" required />
-               
-
-                <div data-netlify-recaptcha="true"></div>
-                <br/>
-                <input type="checkbox" id="Rules agreement" name="Rules agreement" required/>
-                <label htmlFor="Rules agreement">I hereby confirm the photograph I am submitting is an original work <span style={{fontWeight: 'bold'}}> photographed by me between July 9th, 2020 and August 1st, 2020. </span></label>
-                <br/>
-                <br/>
-                <input type="checkbox" id="Rules agreement2" name="Rules agreement2" required/>
-                <label htmlFor="Rules agreement2">I agree to the rules and terms and conditions outlined in the <Link to="/details">details page</Link>.</label>
-
-            <ul style={{paddingTop: '20px'}}className="actions">
-                <li>
-                    <Button type="submit" value="Submit" className="special" />
-                </li>
-            </ul>
-        </StyledForm>
+        <p>
+            <label>Your Name: <input type="text" name="name"/></label>
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email"/></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
         </StyledFormWrapper>
     )
 }
