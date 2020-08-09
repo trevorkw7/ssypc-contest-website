ca314c796b2254cc24844e90aa9fa07db1158669
trevorkw7 <54345106+trevorkw7@users.noreply.github.com>
2020-08-08 23:38:18 -0700

    Update submit.js

--- src/pages/submit.js
+++ src/pages/submit.js
@@ -1,25 +1,25 @@
-// import React from 'react'
-// import Title from 'components/title';
-// import Layout from 'components/layout';
-// import Box from 'components/box';
-// import Form from 'components/form'
-// import { Link } from 'gatsby';
+import React from 'react'
+import Title from 'components/title';
+import Layout from 'components/layout';
+import Box from 'components/box';
+import Form from 'components/form'
+import { Link } from 'gatsby';
 
-// export default function SubmitPage() {
-//     return (
-//         <Layout>
-//             <Box>
-//                 <Title>
-//                     First <Link to="/details">read</Link> the rules.
-//                     Then fill out the form and submit your photo below.
+export default function SubmitPage() {
+    return (
+        <Layout>
+            <Box>
+                <Title>
+                    First <Link to="/details">read</Link> the rules.
+                    Then fill out the form and submit your photo below.
                   
-//                 </Title>
-//                 <br/>
-//                 <Title>
-//                 To submit multiple photos (you may submit one photo per category), please re-submit this form with the same name, email, and high school.
-//                 </Title>
-//             </Box>
-//            <Form/>
-//         </Layout>
-//     )
-// }
+                </Title>
+                <br/>
+                <Title>
+                To submit multiple photos (you may submit one photo per category), please re-submit this form with the same name, email, and high school.
+                </Title>
+            </Box>
+           <Form/>
+        </Layout>
+    )
+}
